eda744020ab5a14f3389ee4b3393eee4e43d7023
Garret Akpale <garretakpale@gmail.com>
2023-08-17 04:21:14 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+
+/**
+ * sig_handler - A function to handle ^C interrupt signal
+ * @uuv: Is required for signal function prototype but unused.
+ *
+ * Return: Returns nothing
+ */
+
+static void sig_handler(int uuv)
+{
+	unsigned int sig_flag = 0;
+
+	(void) uuv;
+
+	if (sig_flag == 0)
+		_puts("\n$ ");
+	else
+		_puts("\n");
+}
+
+/**
+ * main - This is the entry function to the shell
+ * @argc: The argument count to main
+ * @argv: An array list of args passed to main
+ * @envp: Pointer to array of environment variables
+ *
+ * Return: 0 or exit status, or ?
+ */
+int main(int argc __attribute__((unused)), char **argv, char **envp)
+{
+	size_t len_buffer = 0;
+	unsigned int is_pipe = 0, i, sig_flag = 0;
+	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
+
+	(void)sig_flag;
+	vars.argv = argv;
+	vars.env = make_env(envp);
+	signal(SIGINT, sig_handler);
+	if (!isatty(STDIN_FILENO))
+		is_pipe = 1;
+	if (is_pipe == 0)
+		_puts("$ ");
+	sig_flag = 0;
+	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
+	{
+		sig_flag = 1;
+		vars.count++;
+		vars.commands = tokenize(vars.buffer, ";");
+		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
+		{
+			vars.av = tokenize(vars.commands[i], "\n \t\r");
+			if (vars.av && vars.av[0])
+				if (check_for_builtins(&vars) == NULL)
+					check_for_path(&vars);
+			free(vars.av);
+		}
+		free(vars.buffer);
+		free(vars.commands);
+		sig_flag = 0;
+		if (is_pipe == 0)
+			_puts("$ ");
+		vars.buffer = NULL;
+	}
+	if (is_pipe == 0)
+		_puts("\n");
+	free_env(vars.env);
+	free(vars.buffer);
+	exit(vars.status);
+}

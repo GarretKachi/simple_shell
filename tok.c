a046b7f645f37b562e19e8b5000cf15a93794825
Garret Akpale <garretakpale@gmail.com>
2023-08-22 15:51:38 -0700

    tokenization

---
+++ tok.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+
+/**
+ * tokenize - This parses a buffer and tokenize its strings
+ * @buffer: buffer holding the strings
+ * @delimiter: The delimiter to tokenize at
+ *
+ * Return: returns a pointer to the tokenized strings.
+ */
+char **tokenize(char *buffer, char *delimiter)
+{
+	char **tokens = NULL;
+	size_t i = 0, mcount = 10;
+
+	if (buffer == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * mcount);
+	if (tokens == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	while ((tokens[i] = new_strtok(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i == mcount)
+		{
+			tokens = _realloc(tokens, &mcount);
+			if (tokens == NULL)
+			{
+				perror("Fatal Error");
+				return (NULL);
+			}
+		}
+		buffer = NULL;
+	}
+	return (tokens);
+}
